824878587e66a670c67860e90b107915226a1dce
gbarrosn <gbarros.n@gmail.com>
2024-07-15 14:22:01 -0300

    voltando pra a tela inicial da tela de entrada

--- src/gui/registrarEntrada.java
+++ src/gui/registrarEntrada.java
@@ -40,6 +40,11 @@ public class registrarEntrada extends javax.swing.JFrame {
         jButtonEntrada.setText("Registrar Entrada");
 
         jButton1.setText("Tela Inicial");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -94,6 +99,13 @@ public class registrarEntrada extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        telaInicial tela = new telaInicial();
+        tela.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments

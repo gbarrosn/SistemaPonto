3c771c9b1a45306265852d3cf2583c5077215a9e
gbarrosn <gbarros.n@gmail.com>
2024-07-16 15:24:38 -0300

    reescrevendo o registro de acordo com os dados que ficam no banco

--- src/model/registro.java
+++ src/model/registro.java
@@ -9,190 +9,110 @@ package model;
  * @author gbarrosn
  */
 public class registro {
-        private String id;
-        private String name;
-        private String setor;
-        private String turno;
-        private String beginTime;
-        private String endTime;
-        private String date;
-        private String funcao;
-        private String dataAdmissao;
-        private String escala = "";
-        private String horario = "";
-        private String horasSemanais = "";
-        private String codigoDeBarras = "";
-        private String dataAssinatura = "";
-        private String horaAssinatura = "";
-        private String nomeAssinaturaCoordenacao = "";
-        private String horaAssinaturaCoordenacao = "";
-        private String dataAssinaturaCoordenacao = "";
+    int idRegistro;
+    String setor;
+    String turno;
+    String funcao;
+    int idFuncionario;
+    String horaEntrada;
+    String saidaAlmoco;
+    String retornoAlmoco;
+    String horaSaida;
+    String data;
 
-        public registro(String id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-        public registro(String id, String name, String setor, String turno, String funcao, String dataAdmissao, String escala, String horario, String horasSemanais, String codigoDeBarras, String dataAssinatura, String horaAssinatura) {
-            this.id = id;
-            this.name = name;
-            this.setor = setor;
-            this.turno = turno;
-            this.funcao = funcao;
-            this.dataAdmissao = dataAdmissao;
-            this.escala = escala;
-            this.horario = horario;
-            this.horasSemanais = horasSemanais;
-            this.codigoDeBarras = codigoDeBarras;
-            this.dataAssinatura = dataAssinatura;
-            this.horaAssinatura = horaAssinatura;
-        }
+    public registro(int idRegistro, String setor, String turno, String funcao, int idFuncionario, String horaEntrada, String saidaAlmoco, String retornoAlmoco, String horaSaida, String data) {
+        this.idRegistro = idRegistro;
+        this.setor = setor;
+        this.turno = turno;
+        this.funcao = funcao;
+        this.idFuncionario = idFuncionario;
+        this.horaEntrada = horaEntrada;
+        this.saidaAlmoco = saidaAlmoco;
+        this.retornoAlmoco = retornoAlmoco;
+        this.horaSaida = horaSaida;
+        this.data = data;
+    }
 
-        public String getNomeAssinaturaCoordenacao() {
-            return nomeAssinaturaCoordenacao;
-        }
+    // Getters
+    public int getIdRegistro() {
+        return idRegistro;
+    }
 
-        public void setNomeAssinaturaCoordenacao(String nomeAssinaturaCoordenacao) {
-            this.nomeAssinaturaCoordenacao = nomeAssinaturaCoordenacao;
-        }
+    public String getSetor() {
+        return setor;
+    }
 
-        public String getHoraAssinaturaCoordenacao() {
-            return horaAssinaturaCoordenacao;
-        }
+    public String getTurno() {
+        return turno;
+    }
 
-        public void setHoraAssinaturaCoordenacao(String horaAssinaturaCoordenacao) {
-            this.horaAssinaturaCoordenacao = horaAssinaturaCoordenacao;
-        }
+    public String getFuncao() {
+        return funcao;
+    }
 
-        public String getDataAssinaturaCoordenacao() {
-            return dataAssinaturaCoordenacao;
-        }
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
 
-        public void setDataAssinaturaCoordenacao(String dataAssinaturaCoordenacao) {
-            this.dataAssinaturaCoordenacao = dataAssinaturaCoordenacao;
-        }
+    public String getHoraEntrada() {
+        return horaEntrada;
+    }
 
-        public String getDataAssinatura() {
-            return dataAssinatura;
-        }
+    public String getSaidaAlmoco() {
+        return saidaAlmoco;
+    }
 
-        public void setDataAssinatura(String dataAssinatura) {
-            this.dataAssinatura = dataAssinatura;
-        }
+    public String getRetornoAlmoco() {
+        return retornoAlmoco;
+    }
 
-        public String getHoraAssinatura() {
-            return horaAssinatura;
-        }
+    public String getHoraSaida() {
+        return horaSaida;
+    }
 
-        public void setHoraAssinatura(String horaAssinatura) {
-            this.horaAssinatura = horaAssinatura;
-        }
+    public String getData() {
+        return data;
+    }
 
-        public String getCodigoDeBarras() {
-            return codigoDeBarras;
-        }
+    // Setters
+    public void setIdRegistro(int idRegistro) {
+        this.idRegistro = idRegistro;
+    }
 
-        public void setCodigoDeBarras(String codigoDeBarras) {
-            this.codigoDeBarras = codigoDeBarras;
-        }
-        
-        public String getHorasSemanais() {
-            return horasSemanais;
-        }
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
 
-        public void setHorasSemanais(String horasSemanais) {
-            this.horasSemanais = horasSemanais;
-        }
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
 
-        public String getEscala() {
-            return escala;
-        }
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
 
-        public void setEscala(String escala) {
-            this.escala = escala;
-        }
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
 
-        public String getHorario() {
-            return horario;
-        }
+    public void setHoraEntrada(String horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
 
-        public void setHorario(String horario) {
-            this.horario = horario;
-        }
+    public void setSaidaAlmoco(String saidaAlmoco) {
+        this.saidaAlmoco = saidaAlmoco;
+    }
 
-        public String getDataAdmissao() {
-            return dataAdmissao;
-        }
+    public void setRetornoAlmoco(String retornoAlmoco) {
+        this.retornoAlmoco = retornoAlmoco;
+    }
 
-        public void setDataAdmissao(String dataAdmissao) {
-            this.dataAdmissao = dataAdmissao;
-        }
+    public void setHoraSaida(String horaSaida) {
+        this.horaSaida = horaSaida;
+    }
 
-        public String getFuncao() {
-            return funcao;
-        }
+    public void setData(String data) {
+        this.data = data;
+    }
 
-        public void setFuncao(String funcao) {
-            this.funcao = funcao;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getSetor() {
-            return setor;
-        }
-
-        public String getTurno() {
-            return turno;
-        }
-
-        public String getBeginTime() {
-            return beginTime;
-        }
-
-        public String getEndTime() {
-            return endTime;
-        }
-
-        public String getDate(){
-            return date;
-        }
-
-        public void setSetor(String setor) {
-            this.setor = setor;
-        }        
-
-        public void setTurno(String turno) {
-            this.turno = turno;
-        }
-
-        public void setBeginTime(String beginTime) {
-            this.beginTime = beginTime;
-        }       
-
-        public void setEndTime(String endTime) {
-            this.endTime = endTime;
-        }        
-
-        public void setDate(String date) {
-            this.date = date;
-        }
-
-        @Override
-        public String toString() {
-            return "Registro{" +
-                    "id='" + id + '\'' +
-                    ", name='" + name + '\'' +
-                    ", setor='" + setor + '\'' +
-                    ", turno='" + turno + '\'' + 
-                    ", beginTime='" + beginTime + '\'' +
-                    ", endTime='" + endTime + '\'' +
-                    ", date='" + date + '\'' +
-                    '}';
-        }
 }

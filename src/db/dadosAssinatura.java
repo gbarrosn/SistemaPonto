1a90ecce8ba0c2e8c5b7753f26b034e9745840d0
gbarrosn <gbarros.n@gmail.com>
2024-07-17 16:12:28 -0300

    registrando assinatura

--- src/db/dadosAssinatura.java
+++ src/db/dadosAssinatura.java
@@ -8,5 +8,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class dadosAssinatura {
-    
+    //função que registra uma assinatura no banco de dados usando conectarBanco e assinatura
+    public void registrarAssinatura(int idFuncionario, int mes, String dataAssinatura, String horaAssinatura) throws SQLException {
+        // Connect to the database
+        Connection connection = conectarBanco.conectar();
+        
+        // Create a query to insert the assinatura
+        String query = "INSERT INTO assinaturas (id_funcionario, mes, data_assinatura, hora_assinatura) VALUES (" + idFuncionario + ", " + mes + ", '" + dataAssinatura + "', '" + horaAssinatura + "')";
+        
+        try {
+            // Create a statement
+            Statement statement = connection.createStatement();
+            
+            // Execute the query
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
 }

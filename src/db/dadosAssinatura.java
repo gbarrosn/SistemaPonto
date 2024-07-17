71dc78a5233523955b43a41bbc15fea7a5b6b5ca
gbarrosn <gbarros.n@gmail.com>
2024-07-17 16:11:08 -0300

    criando classe dados assinatura

---
+++ src/db/dadosAssinatura.java
@@ -0,0 +1,12 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class dadosAssinatura {
+    
+}

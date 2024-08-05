a4ad2af59a49e27ccfe63e40e5f8f7cbbfa500f0
gbarrosn <gbarros.n@gmail.com>
2024-08-05 15:11:43 -0300

    criando query da assinatura da coordenação

--- src/db/dadosAssinaturaCoordenacao.java
+++ src/db/dadosAssinaturaCoordenacao.java
@@ -4,10 +4,40 @@
  */
 package db;
 
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 /**
  *
  * @author gbarrosn
  */
 public class dadosAssinaturaCoordenacao {
     
+    public static void registrarAssinaturaCoordenacao(int idCoordenacao, int idFuncionario, String dataAssinatura, String horaAssinatura, int mes) {
+        // Connect to the database
+        Connection connection = null;
+        try {
+            connection = conectarBanco.conectar();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        // Create a query to insert the assinatura
+        String query = "INSERT INTO assinaturaCoordenacao (id_coordenacao, id_funcionario, data_assinatura, hora_assinatura, mes) VALUES (" + idCoordenacao + ", " + idFuncionario +  ", '" + dataAssinatura + "', '" + horaAssinatura + "', " + mes +")";
+        
+        try {
+            // Create a statement
+            Statement statement = connection.createStatement();
+            
+            // Execute the query
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
 }

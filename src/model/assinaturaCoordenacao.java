04f9ddd651234e4647ed84f1b09fd79bc96c21b6
gbarrosn <gbarros.n@gmail.com>
2024-08-05 15:38:38 -0300

    criando classe da assinatua da coordenação

--- src/model/assinaturaCoordenacao.java
+++ src/model/assinaturaCoordenacao.java
@@ -9,5 +9,58 @@ package model;
  * @author gbarrosn
  */
 public class assinaturaCoordenacao {
-    
+    private int idAssinatura;
+    private int idFuncionario;
+    private int idCoordenacao;
+    private int mes;
+    private String dataAssinatura;
+    private String horaAssinatura;
+
+    public int getIdAssinatura() {
+        return idAssinatura;
+    }
+
+    public void setIdAssinatura(int idAssinatura) {
+        this.idAssinatura = idAssinatura;
+    }
+
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public int getIdCoordenacao() {
+        return idCoordenacao;
+    }
+
+    public void setIdCoordenacao(int idCoordenacao) {
+        this.idCoordenacao = idCoordenacao;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public String getDataAssinatura() {
+        return dataAssinatura;
+    }
+
+    public void setDataAssinatura(String dataAssinatura) {
+        this.dataAssinatura = dataAssinatura;
+    }
+
+    public String getHoraAssinatura() {
+        return horaAssinatura;
+    }
+
+    public void setHoraAssinatura(String horaAssinatura) {
+        this.horaAssinatura = horaAssinatura;
+    }
 }

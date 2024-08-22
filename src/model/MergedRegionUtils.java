e52d1969c38e809a4de9bd37166a1ee96a420565
gbarrosn <gbarros.n@gmail.com>
2024-08-22 15:46:29 -0300

    criando classe para identificar celulas mescladas

---
+++ src/model/MergedRegionUtils.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author gbarrosn
+ */
+
+
+public class MergedRegionUtils {
+
+    public static List<MergedRegion> identifyMergedRegions(Sheet sheet) {
+        List<MergedRegion> mergedRegions = new ArrayList<>();
+        int numMergedRegions = sheet.getNumMergedRegions();
+
+        for (int i = 0; i < numMergedRegions; i++) {
+            org.apache.poi.ss.util.CellRangeAddress region = sheet.getMergedRegion(i);
+            mergedRegions.add(new MergedRegion(region.getFirstRow(), region.getLastRow(), region.getFirstColumn(), region.getLastColumn()));
+        }
+
+        return mergedRegions;
+    }
+
+    public static boolean isMergedCell(Cell cell, List<MergedRegion> mergedRegions) {
+        for (MergedRegion region : mergedRegions) {
+            if (region.contains(cell.getRowIndex(), cell.getColumnIndex())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int getMergedRegionColumns(Cell cell, List<MergedRegion> mergedRegions) {
+        for (MergedRegion region : mergedRegions) {
+            if (region.contains(cell.getRowIndex(), cell.getColumnIndex())) {
+                return region.getLastColumn() - region.getFirstColumn() + 1;
+            }
+        }
+        return 1; // If the cell is not merged, it spans 1 column
+    }
+
+    private static class MergedRegion {
+        private final int firstRow;
+        private final int lastRow;
+        private final int firstColumn;
+        private final int lastColumn;
+
+        public MergedRegion(int firstRow, int lastRow, int firstColumn, int lastColumn) {
+            this.firstRow = firstRow;
+            this.lastRow = lastRow;
+            this.firstColumn = firstColumn;
+            this.lastColumn = lastColumn;
+        }
+
+        public boolean contains(int rowIndex, int columnIndex) {
+            return firstRow <= rowIndex && rowIndex <= lastRow && firstColumn <= columnIndex && columnIndex <= lastColumn;
+        }
+    }
+}

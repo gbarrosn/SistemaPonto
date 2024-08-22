cca782ea6a70e023423d632afb6184c0b5c5e6e4
gbarrosn <gbarros.n@gmail.com>
2024-08-22 14:12:13 -0300

    carregando o mes certo

--- src/gui/gerarFolhas.java
+++ src/gui/gerarFolhas.java
@@ -291,7 +291,7 @@ public class gerarFolhas extends javax.swing.JFrame {
         meses.add("Novembro");
         meses.add("Dezembro");
 
-        return meses.get(mes);
+        return meses.get(mes-1);
     }
     /**
      * @param args the command line arguments

7fff88f4abcaaf65cf9d3760ac3fe8bf63889eb3
gbarrosn <gbarros.n@gmail.com>
2024-08-26 17:06:25 -0300

    cadastrando contrato

--- src/db/dadosContrato.java
+++ src/db/dadosContrato.java
@@ -4,10 +4,47 @@
  */
 package db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author gbarrosn
  */
 public class dadosContrato {
+    
+    public static void cadastrarContrato(String contrato) throws SQLException {
+        // Connect to the database
+        Connection connection = conectarBanco.conectar();
+
+        // Verificar se o contrato já existe
+        String checkQuery = "SELECT * FROM contrato WHERE contrato = ?";
+        PreparedStatement checkStatement = connection.prepareStatement(checkQuery);
+        checkStatement.setString(1, contrato);
+        ResultSet resultSet = checkStatement.executeQuery();
+
+        if (resultSet.next()) {
+            // Contrato já existe, então atualizamos
+            String updateQuery = "UPDATE contrato SET contrato = ? WHERE contrato = ?";
+            PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
+            updateStatement.setString(1, contrato);
+            updateStatement.setString(2, contrato);
+            updateStatement.executeUpdate();
+        } else {
+            // Contrato não existe, então inserimos
+            String insertQuery = "INSERT INTO contrato (contrato) VALUES (?)";
+            PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
+            insertStatement.setString(1, contrato);
+            insertStatement.executeUpdate();
+        }
+
+        // Feche os recursos
+        resultSet.close();
+        checkStatement.close();
+        connection.close();
+    }
+
     
 }

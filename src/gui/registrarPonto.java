d5414ed2717ac6e3b8cb46d4d75a700aa5899a64
gbarrosn <gbarros.n@gmail.com>
2024-10-04 14:06:34 -0300

    atualizando msgs e refatorando

--- src/gui/registrarPonto.java
+++ src/gui/registrarPonto.java
@@ -183,7 +183,7 @@ public class registrarPonto extends javax.swing.JFrame {
 
             try {
                 dadosRegistro.registrarPonto(funcionarioRegistro.getIdFuncionario(), hora, data);
-                JOptionPane.showMessageDialog(this, "Saída registrada!");
+                JOptionPane.showMessageDialog(this, "Ponto registrado!");
                 jTextFieldMatricula.setText("");
             } catch (SQLException e) {
                 // TODO Auto-generated catch block
